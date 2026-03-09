9b8424b27d79f9bde2d7223cc5e347007509ce6e
Serhii Shimko <shimkosergij2010@gmail.com>
2026-03-09 23:28:33 +0200

    completed the task

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -22,23 +22,257 @@ class Game {
    */
   constructor(initialState) {
     // eslint-disable-next-line no-console
-    console.log(initialState);
+    this.button = document.querySelector('.start');
+    this.table = document.querySelector('.game-field');
+    this.score = document.querySelector('.game-score');
+    this.allRow = Array.from(this.table.rows);
+    this.arrayAllCells = Array.from(document.querySelectorAll('.field-cell'));
+    this.move = true;
+    this.messageStart = document.querySelector('.message-start');
+    this.messageLose = document.querySelector('.message-lose');
+    this.messageWin = document.querySelector('.message-win');
+    this.bestRecord = 0;
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  moveLeft() {
+    this.move = false;
+
+    for (const row of this.allRow) {
+      const cellsRow = row.cells;
+      const newArray = [];
+
+      for (let index = cellsRow.length - 1; index >= 0; index--) {
+        const element = cellsRow[index];
+        const lastElement = cellsRow[index - 1];
+
+        if (lastElement && element.textContent !== '') {
+          if (lastElement.textContent === '') {
+            this.move = true;
+          }
+        }
+
+        if (element.textContent !== '') {
+          newArray.push(element);
+        }
+      }
+
+      newArray.forEach((td) => {
+        row.prepend(td);
+      });
+
+      for (let i = cellsRow.length - 1; i >= 0; i--) {
+        const element = cellsRow[i];
+        const lastElement = cellsRow[i - 1];
+
+        if (lastElement && element.textContent !== '') {
+          if (element.textContent === lastElement.textContent) {
+            const n = Number(lastElement.textContent) * 2;
+
+            element.textContent = '';
+            row.append(element);
+
+            const classElement = element.getAttribute('class');
+            const secondClassElement = classElement.split(' ')[1];
+            const classLastElement = lastElement.getAttribute('class');
+            const secondClassLastElement = classLastElement.split(' ')[1];
+
+            i--;
+            this.move = true;
+
+            if (secondClassElement && secondClassLastElement) {
+              element.classList.remove(secondClassElement);
+              lastElement.classList.remove(secondClassLastElement);
+            }
+            lastElement.textContent = String(n);
+            lastElement.classList.add(`field-cell--${n}`);
+          }
+        }
+      }
+    }
+  }
+  moveRight() {
+    this.move = false;
+
+    for (const row of this.allRow) {
+      const cellsRow = row.cells;
+      const newArray = [];
+
+      for (let index = 0; index < cellsRow.length; index++) {
+        const element = cellsRow[index];
+        const nextElement = cellsRow[index + 1];
+
+        if (nextElement && element.textContent !== '') {
+          if (nextElement.textContent === '') {
+            this.move = true;
+          }
+        }
+
+        if (element.textContent !== '') {
+          newArray.push(element);
+        }
+      }
+
+      newArray.forEach((td) => {
+        row.append(td);
+      });
+
+      for (let i = 0; i < cellsRow.length; i++) {
+        const element = cellsRow[i];
+        const nextElement = cellsRow[i + 1];
+
+        if (nextElement && element.textContent !== '') {
+          if (element.textContent === nextElement.textContent) {
+            const n = Number(nextElement.textContent) * 2;
+
+            element.textContent = '';
+            row.prepend(element);
+
+            const classElement = element.getAttribute('class');
+            const secondClassElement = classElement.split(' ')[1];
+            const classNextElement = nextElement.getAttribute('class');
+            const secondClassLastElement = classNextElement.split(' ')[1];
+
+            i++;
+            this.move = true;
+
+            if (secondClassElement && secondClassLastElement) {
+              element.classList.remove(secondClassElement);
+              nextElement.classList.remove(secondClassLastElement);
+            }
+            nextElement.textContent = String(n);
+            nextElement.classList.add(`field-cell--${n}`);
+          }
+        }
+      }
+    }
+  }
+  moveUp() {
+    this.move = false;
+
+    for (let i = 0; i < 4; i++) {
+      const columnRow = [];
+
+      for (const row of this.allRow) {
+        columnRow.push(row.cells[i]);
+      }
+
+      const newArray = [];
+
+      columnRow.forEach((td) => {
+        if (td.textContent !== '') {
+          newArray.push(td.textContent);
+        }
+      });
+
+      for (let n = newArray.length - 1; n >= 0; n--) {
+        const element = newArray[n];
+        const lastElement = newArray[n - 1];
+
+        if (lastElement && lastElement === element) {
+          newArray[n - 1] = String(Number(lastElement) * 2);
+          newArray.splice(n, 1);
+          this.move = true;
+          n--;
+        }
+      }
+
+      for (let j = 0; j < columnRow.length; j++) {
+        const element = columnRow[j];
+        const newElement = newArray[j];
+
+        if (newElement && element.textContent !== newElement) {
+          this.move = true;
+        }
+
+        if (newElement) {
+          element.className = 'field-cell';
+          element.classList.add(`field-cell--${newElement}`);
+          element.textContent = newElement;
+        } else {
+          element.className = 'field-cell';
+          element.textContent = '';
+        }
+      }
+    }
+  }
+  moveDown() {
+    this.move = false;
+
+    for (let i = 0; i < 4; i++) {
+      const columnRow = [];
+
+      for (const row of this.allRow) {
+        columnRow.push(row.cells[i]);
+      }
+
+      const newArray = [];
+
+      columnRow.forEach((td) => {
+        if (td.textContent !== '') {
+          newArray.push(td.textContent);
+        }
+      });
+
+      for (let n = 0; n < newArray.length; n++) {
+        const element = newArray[n];
+        const NextElement = newArray[n + 1];
+
+        if (NextElement && NextElement === element) {
+          newArray[n + 1] = String(Number(NextElement) * 2);
+          newArray.splice(n, 1);
+          this.move = true;
+          n++;
+        }
+      }
+
+      let count = newArray.length - 1;
+
+      for (let j = columnRow.length - 1; j >= 0; j--) {
+        const element = columnRow[j];
+        const newElement = newArray[count];
+
+        count--;
+
+        if (newElement && element.textContent !== newElement) {
+          this.move = true;
+        }
+
+        if (newElement) {
+          element.className = 'field-cell';
+          element.classList.add(`field-cell--${newElement}`);
+          element.textContent = newElement;
+        } else {
+          element.className = 'field-cell';
+          element.textContent = '';
+        }
+      }
+    }
+  }
 
   /**
    * @returns {number}
    */
-  getScore() {}
+  getScore() {
+    let score = 0;
+
+    this.arrayAllCells.forEach((td) => {
+      const content = Number(td.textContent);
+
+      score += content;
+    });
+
+    this.score.textContent = score;
+    this.bestRecord = score;
+  }
 
   /**
    * @returns {number[][]}
    */
-  getState() {}
+  getState() {
+    const state = [this.bestRecord, this.score.textContent];
+
+    return state;
+  }
 
   /**
    * Returns the current game status.
@@ -50,19 +284,150 @@ class Game {
    * `win` - the game is won;
    * `lose` - the game is lost
    */
-  getStatus() {}
+  getStatus() {
+    const result = this.arrayAllCells.filter((td) => {
+      if (td.textContent !== '') {
+        return true;
+      }
+    });
+
+    if (result.length === 0) {
+      return 'idle';
+    }
+
+    if (result.includes('2048')) {
+      return 'win';
+    }
+
+    if (result.length === 16) {
+      return 'lose';
+    }
+
+    return `playing`;
+  }
 
   /**
    * Starts the game.
    */
-  start() {}
+
+  start() {
+    this.button.classList.remove('start');
+    this.button.classList.add('restart');
+    this.button.textContent = 'Restart';
+
+    let count = 0;
+
+    while (count === 0) {
+      const cells = this.arrayAllCells;
+      const lengthAllCells = cells.length;
+      const randomIndexFirst = Math.floor(Math.random() * lengthAllCells);
+      const randomIndexSecond = Math.floor(Math.random() * lengthAllCells);
+
+      if (randomIndexFirst !== randomIndexSecond) {
+        let content;
+
+        if (Math.random() < 0.1) {
+          content = 4;
+        } else {
+          content = 2;
+        }
+        cells[randomIndexFirst].textContent = `${content}`;
+        cells[randomIndexFirst].classList.add(`field-cell--${content}`);
+        cells[randomIndexSecond].textContent = `${content}`;
+        cells[randomIndexSecond].classList.add(`field-cell--${content}`);
+        count++;
+      }
+    }
+  }
 
   /**
    * Resets the game.
    */
-  restart() {}
+  restart() {
+    if (!this.messageWin.classList.contains('hidden')) {
+      this.messageWin.classList.add('hidden');
+    }
 
-  // Add your own methods here
+    if (!this.messageLose.classList.contains('hidden')) {
+      this.messageLose.classList.add('hidden');
+    }
+    this.messageStart.classList.remove('hidden');
+
+    this.arrayAllCells.forEach((td) => {
+      if (td.textContent !== '') {
+        const classGet = td.getAttribute('class');
+        const secondClass = classGet.split(' ')[1];
+
+        td.textContent = '';
+
+        if (secondClass) {
+          td.classList.remove(secondClass);
+        }
+      }
+    });
+
+    let count = 0;
+
+    while (count === 0) {
+      const cells = this.arrayAllCells;
+      const lengthAllCells = cells.length;
+      const randomIndexFirst = Math.round(
+        0 + Math.random() * (lengthAllCells - 1),
+      );
+      const randomIndexSecond = Math.round(
+        0 + Math.random() * (lengthAllCells - 1),
+      );
+
+      if (randomIndexFirst !== randomIndexSecond) {
+        let content = 2;
+
+        if (Math.random() < 0.1) {
+          content = 4;
+        } else {
+          content = 2;
+        }
+        cells[randomIndexFirst].textContent = `${content}`;
+        cells[randomIndexFirst].classList.add(`field-cell--${content}`);
+        cells[randomIndexSecond].textContent = `${content}`;
+        cells[randomIndexSecond].classList.add(`field-cell--${content}`);
+        count++;
+      }
+    }
+  }
+
+  newElement() {
+    const newArray = this.arrayAllCells.filter((td) => {
+      if (td.textContent === '') {
+        return true;
+      }
+    });
+
+    if (newArray.includes('2048')) {
+      this.messageStart.classList.add('hidden');
+      this.messageWin.classList.remove('hidden');
+    }
+
+    if (newArray.length === 0) {
+      this.messageStart.classList.add('hidden');
+      this.messageLose.classList.remove('hidden');
+    }
+
+    if (this.move === true) {
+      let content = 2;
+      const random = Math.floor(Math.random() * newArray.length);
+      const element = newArray[random];
+
+      if (element instanceof HTMLElement) {
+        if (Math.random() < 0.1) {
+          content = 4;
+        } else {
+          content = 2;
+        }
+        element.textContent = `${content}`;
+        element.classList.add(`field-cell--${content}`);
+      }
+    }
+  }
 }
 
 module.exports = Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,45 @@
 'use strict';
 
 // Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
+const game = new Game();
 
-// Write your code here
+const button = document.querySelector('.start');
+
+button.addEventListener('click', (e) => {
+  if (button.classList.contains('start')) {
+    game.start();
+    game.getScore();
+  } else {
+    game.restart();
+    game.getScore();
+  }
+});
+
+document.addEventListener('keydown', (e) => {
+  e.preventDefault();
+
+  if (e.key === 'ArrowLeft') {
+    game.moveLeft();
+    game.newElement();
+    game.getScore();
+  }
+
+  if (e.key === 'ArrowRight') {
+    game.moveRight();
+    game.newElement();
+    game.getScore();
+  }
+
+  if (e.key === 'ArrowUp') {
+    game.moveUp();
+    game.newElement();
+    game.getScore();
+  }
+
+  if (e.key === 'ArrowDown') {
+    game.moveDown();
+    game.newElement();
+    game.getScore();
+  }
+});
